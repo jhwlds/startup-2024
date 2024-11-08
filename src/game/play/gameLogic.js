9990b7c6ac074b85d0cf3365150e430109e54718
Howon Jung <jhwlds@gmail.com>
2024-11-08 02:07:16 -0700

    adding rank box

--- src/game/play/gameLogic.js
+++ src/game/play/gameLogic.js
@@ -18,7 +18,23 @@ export const useGameLogic = () => {
             setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
         } else {
             setIsGameOver(true);
+            updateRanks();
         }
+    };
+
+    // 랭킹 업데이트 함수
+    const updateRanks = () => {
+        const userName = new URLSearchParams(window.location.search).get('name');
+        const newScore = { name: userName, score };
+        
+        const savedScores = JSON.parse(localStorage.getItem('topScores')) || [];
+        
+        // 점수 배열에 새 점수 추가 후 내림차순 정렬
+        const updatedScores = [...savedScores, newScore]
+            .sort((a, b) => b.score - a.score)
+            .slice(0, 5); // 상위 5명만 유지
+        
+        localStorage.setItem('topScores', JSON.stringify(updatedScores));
     };
 
     return { questions, currentQuestionIndex, score, isGameOver, handleAnswerClick };
