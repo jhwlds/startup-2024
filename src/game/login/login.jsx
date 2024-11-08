17526b3a832c6fee76ac5a58ad1788956dfca677
Howon Jung <jhwlds@gmail.com>
2024-11-08 00:25:19 -0700

    log in enter key

--- src/game/login/login.jsx
+++ src/game/login/login.jsx
@@ -1,17 +1,26 @@
-
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './login.css';
 
 export function Login() {
+    const navigate = useNavigate();
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            navigate('/game/play');
+        }
+    };
 
     return (
         <header>
             <div className='login-box'>
                 <div>
                     <label>Log In</label>
-                    <input type="text" placeholder="Enter your name" />
+                    <input 
+                        type="text" 
+                        placeholder="Enter your name" 
+                        onKeyDown={handleKeyDown}
+                    />
                     <NavLink to='/game/play'>
                         Log In
                     </NavLink>
@@ -20,4 +29,3 @@ export function Login() {
         </header>
     );
 }
-
